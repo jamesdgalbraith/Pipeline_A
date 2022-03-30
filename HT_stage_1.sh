96b6eb148f84de2bf954304fe86ee914bac87936
jamesdgalbraith <galbraithenator@gmail.com>
2022-03-30 09:40:56 +0100

    Corrected blast task to dc-megablast

--- HT_stage_1.sh
+++ HT_stage_1.sh
@@ -17,8 +17,8 @@ Rscript splitter.R -f ${QUERY}.centroids -p ${THREADS} -t DNA
 ls split/${QUERY}.centroids_seq_* | sed 's/.*\///' > split_seq_list.txt
 
 ### search genome in parallel. do for source and outgroup(s)
-parallel --bar --jobs ${THREADS} -a split_seq_list.txt 'blastn -query split/{} -db seq/${GENOME} -out data/{}_${GENOME}.out -outfmt "6 std qlen slen"'
-parallel --bar --jobs ${THREADS} -a split_seq_list.txt 'blastn -query split/{} -db seq/${OUTGROUP} -out data/{}_${OUTGROUP}.out -outfmt "6 std qlen slen"'
+parallel --bar --jobs ${THREADS} -a split_seq_list.txt 'blastn -query split/{} -db seq/${GENOME} -out data/{}_${GENOME}.out -outfmt "6 std qlen slen" -task dc-megablast'
+parallel --bar --jobs ${THREADS} -a split_seq_list.txt 'blastn -query split/{} -db seq/${OUTGROUP} -out data/{}_${OUTGROUP}.out -outfmt "6 std qlen slen" -task dc-megablast'
 
 ### compile blast out
 cat data/${QUERY}.centroids_seq_*${GENOME}.out > data/${QUERY}.centroids_${GENOME}.out
--- HT_stage_2.sh
+++ HT_stage_2.sh
@@ -10,8 +10,8 @@ Rscript splitter.R -f ${QUERY} -p ${THREADS} -t DNA
 ls split/${QUERY}_seq_* | sed 's/.*\///' > split_seq_list.txt
 
 ### search genome in parallel. do for source and outgroup(s)
-parallel --bar --jobs ${THREADS} -a split_seq_list.txt 'blastn -query split/{} -db seq/${GENOME} -out data/{}_${GENOME}.out -outfmt "6 std qlen slen"'
-parallel --bar --jobs ${THREADS} -a split_seq_list.txt 'blastn -query split/{} -db seq/${OUTGROUP} -out data/{}_${OUTGROUP}.out -outfmt "6 std qlen slen"'
+parallel --bar --jobs ${THREADS} -a split_seq_list.txt 'blastn -query split/{} -db seq/${GENOME} -out data/{}_${GENOME}.out -outfmt "6 std qlen slen" -task dc-megablast'
+parallel --bar --jobs ${THREADS} -a split_seq_list.txt 'blastn -query split/{} -db seq/${OUTGROUP} -out data/{}_${OUTGROUP}.out -outfmt "6 std qlen slen" -task dc-megablast'
 
 ### compile blast out
 cat data/${QUERY}_seq_*${GENOME}.out > data/${QUERY}_${GENOME}.out
